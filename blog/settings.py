6d24ba4def5488715c88df6be633ddbf1533de0e
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-04-08 01:39:35 +0000

    added whiitenoise to requeirments.txt

--- blog/settings.py
+++ blog/settings.py
@@ -54,6 +54,7 @@ AUTH_USER_MODEL = 'blogsite.CustomUser' # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
